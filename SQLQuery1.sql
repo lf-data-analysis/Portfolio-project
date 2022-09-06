de31b2db2c9bf979ce5958572871ac794c4f3403
lf-data-analysis <112306424+lf-data-analysis@users.noreply.github.com>
2022-09-06 10:48:59 +0100

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,163 @@
+SELECT *
+FROM [Portfolio Project]..CovidDeaths$
+ORDER BY 3,4
+
+SELECT *
+FROM [Portfolio Project]..CovidVaccinations$
+ORDER BY 3,4
+
+SELECT Location, date, total_cases, new_cases,total_deaths,population
+FROM [Portfolio Project]..CovidDeaths$
+ORDER BY 1,2
+
+---Percentage of total cases that result in death in the UK
+SELECT Location, date, total_cases,total_deaths, (total_deaths/total_cases)*100 as DeathPercentage
+FROM [Portfolio Project]..CovidDeaths$
+WHERE location like 'United Kingdom'
+ORDER BY 1,2
+
+---Percentage of the population who has had covid ind the UK
+SELECT Location, date, total_cases, Population, (total_cases/Population)*100 as PopulationInfectionRate
+FROM [Portfolio Project]..CovidDeaths$
+WHERE location like 'United Kingdom'
+ORDER BY 1,2
+
+---Countries with highest infection rate
+SELECT Location, Population, MAX(total_cases) as HighestCases, Max((total_cases/Population))*100 as PopulationInfectionRate
+FROM [Portfolio Project]..CovidDeaths$
+GROUP BY Location, population
+ORDER BY 4 DESC
+
+---Countries with highest death rate per capita
+SELECT Location, Population, MAX(total_deaths) AS HighestTotalDeaths, MAX((total_deaths/Population))*100 as PopulationDeathRate
+FROM [Portfolio Project]..CovidDeaths$ WHERE continent IS NOT NULL
+GROUP BY Location, population
+ORDER BY 3 DESC
+
+---Countries with highest death count
+SELECT Location, MAX(Cast(total_deaths AS int)) AS DeathCount
+FROM [Portfolio Project]..CovidDeaths$ WHERE continent IS NOT NULL
+GROUP BY Location
+ORDER BY DeathCount DESC
+
+---Continent with highest death rate per capita
+SELECT continent, MAX((CAST(total_deaths AS INT)) as TotalDeaths, Population, Max((total_deaths/Population))*100 as PopulationDeathRate
+FROM [Portfolio Project]..CovidDeaths$
+GROUP BY Continent, Population
+ORDER BY 4 DESC
+
+---Continent with highest death count
+SELECT Location, MAX(Cast(total_deaths AS int)) AS DeathCount
+FROM [Portfolio Project]..CovidDeaths$
+WHERE Continent IS NULL 
+AND population IS NOT NULL 
+AND location NOT IN ('World', 'Low income', 'Lower middle Income', 'High Income', 'European Union')
+GROUP BY Location
+ORDER BY DeathCount DESC
+
+
+--=Global data
+SELECT date, SUM(CAST(total_cases as int)) as TotalCases , SUM(CAST(total_deaths as int)) as Totaldeaths, 
+(SUM(CAST(total_deaths as float))/SUM(CAST(total_cases as float)))*100 AS DeathPercentage
+FROM [Portfolio Project]..CovidDeaths$
+WHERE continent IS NOT NULL
+GROUP BY date
+ORDER BY 1 asc
+
+---Joining tables
+SELECT *
+FROM [Portfolio Project]..CovidDeaths$ dea
+JOIN [Portfolio Project]..CovidVaccinations$ vac
+      ON dea.location = vac.location
+	  AND dea.date =vac.date
+
+---Global New Vaccines  
+SELECT dea.date, dea.continent, dea.location, vac.new_vaccinations
+FROM [Portfolio Project]..CovidDeaths$ dea
+JOIN [Portfolio Project]..CovidVaccinations$ vac
+      ON dea.location = vac.location
+	  AND dea.date =vac.date
+WHERE dea.location = 'world'
+ORDER BY date
+
+---Global Total Vaccines
+SELECT dea.date, dea.location, vac.total_vaccinations
+FROM [Portfolio Project]..CovidDeaths$ dea
+JOIN [Portfolio Project]..CovidVaccinations$ vac
+      ON dea.location = vac.location
+	  AND dea.date =vac.date
+WHERE dea.location = 'world'
+ORDER BY date
+
+---Percentage of global population vaccinated
+SELECT dea.date, dea.location, vac.people_vaccinated , dea.population ,(vac.people_vaccinated/dea.population)*100 AS VaccinatedPercentage
+FROM [Portfolio Project]..CovidDeaths$ dea
+JOIN [Portfolio Project]..CovidVaccinations$ vac
+      ON dea.location = vac.location
+	  AND dea.date =vac.date
+WHERE dea.location = 'world'
+ORDER BY date
+
+---Rolling count of vaccines by country
+set ansi_warnings off
+SELECT dea.date, dea.continent, dea.location, dea.population, vac.new_vaccinations, vac.people_vaccinated, 
+SUM(CAST(vac.new_vaccinations as bigint)) OVER (PARTITION BY dea.location ORDER BY dea.location, dea.date) AS TotalVaccinations,
+(vac.people_vaccinated/dea.population)*100 AS VaccinationRate
+FROM [Portfolio Project]..CovidDeaths$ dea
+JOIN [Portfolio Project]..CovidVaccinations$ vac
+      ON dea.location = vac.location
+	  AND dea.date =vac.date
+WHERE dea.location LIKE 'United Kingdom'
+ORDER BY dea.location, dea.date
+
+
+---another method
+WITH popvsvac(Continent, Date, Location, Population, New_vaccinations, TotalVacs)
+AS
+(
+SELECT dea.date, dea.continent, dea.location, dea.population, vac.new_vaccinations, 
+SUM(CAST(vac.new_vaccinations as bigint)) OVER (PARTITION BY dea.location ORDER BY dea.location, dea.date) AS TotalVacs
+--(TotalVacs/dea.population)*100 AS VacRate
+FROM [Portfolio Project]..CovidDeaths$ dea
+JOIN [Portfolio Project]..CovidVaccinations$ vac
+      ON dea.location = vac.location
+	  AND dea.date =vac.date
+WHERE dea.location LIKE 'United Kingdom'
+)
+SELECT *, (totalvacs/population) AS VacsPerCapita
+FROM popvsvac
+
+
+---Temp Table
+DROP TABLE IF EXISTS #percentagePopVac
+CREATE TABLE #PercentagePopVac
+(
+Continent nvarchar(255)
+,Location nvarchar(255)
+,Date Datetime
+,Population numeric
+,NewVaccinations numeric
+,RollingVaccinations numeric
+)
+INSERT INTO #PercentagePopVac
+SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations 
+,SUM(CAST(vac.new_vaccinations as bigint)) OVER (PARTITION BY dea.location ORDER BY dea.location, dea.date) AS TotalVaccinations
+FROM [Portfolio Project]..CovidDeaths$ dea
+JOIN [Portfolio Project]..CovidVaccinations$ vac
+      ON dea.location = vac.location
+	  AND dea.date =vac.date
+WHERE dea.location LIKE 'United Kingdom'
+
+SELECT *, (RollingVaccinations/Population) AS VacsPerCapita
+FROM #PercentagePopVac
+
+---Creating View
+CREATE VIEW DeathRateByContinent as
+SELECT Location, MAX(Cast(total_deaths AS int)) AS DeathCount
+FROM [Portfolio Project]..CovidDeaths$
+WHERE Continent IS NULL 
+AND population IS NOT NULL 
+AND location NOT LIKE '%income'
+AND location NOT LIKE '%union'
+AND location NOT LIKE 'world'
+GROUP BY Location
---
+++ house sql.sql
@@ -0,0 +1,161 @@
+SELECT *
+FROM [Portfolio Project]..Sheet1$
+
+
+
+
+SELECT SaleDateConverted, CONVERT(Date,SaleDate)
+FROM [Portfolio Project]..Sheet1$
+
+
+Update [Portfolio Project]..Sheet1$
+SET SaleDate = CONVERT(Date,SaleDate)
+
+ALTER TABLE [Portfolio Project]..Sheet1$
+Add SaleDateConverted Date;
+
+Update [Portfolio Project]..Sheet1$
+SET SaleDateConverted = CONVERT(Date,SaleDate)
+
+
+
+
+---Populate Property Address data
+
+SELECT a.ParcelID, a.PropertyAddress, b.ParcelID, b.PropertyAddress, ISNULL(a.PropertyAddress,b.PropertyAddress)
+FROM [Portfolio Project]..Sheet1$ a
+JOIN [Portfolio Project]..Sheet1$ b
+    ON a.ParcelID = b.ParcelID
+	AND a.[UniqueID ] <> b.[UniqueID ]
+WHERE a.PropertyAddress IS NULL
+
+
+
+UPDATE a
+SET PropertyAddress = ISNULL(a.PropertyAddress,b.PropertyAddress)
+FROM [Portfolio Project]..Sheet1$ a
+JOIN [Portfolio Project]..Sheet1$ b
+    ON a.ParcelID = b.ParcelID
+	AND a.[UniqueID ] <> b.[UniqueID ]
+WHERE a.PropertyAddress IS NULL
+
+
+--- Breaking Out Address into Individual Columns (address, city, state)
+
+SELECT *
+FROM [Portfolio Project]..Sheet1$
+
+SELECT
+SUBSTRING(PropertyAddress, 1, CHARINDEX(',', PropertyAddress) -1 ) AS Address
+, SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress) + 1 ,LEN(PropertyAddress)) AS Address
+
+FROM [Portfolio Project]..Sheet1$
+
+ALTER TABLE [Portfolio Project]..Sheet1$
+Add PropertySplitAddress Nvarchar(255);
+
+Update [Portfolio Project]..Sheet1$
+SET PropertySplitAddress = SUBSTRING(PropertyAddress, 1, CHARINDEX(',', PropertyAddress) -1 )
+
+ALTER TABLE [Portfolio Project]..Sheet1$
+Add PropertySplitCity Nvarchar(255);
+
+Update [Portfolio Project]..Sheet1$
+SET PropertySplitCity = SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress) + 1 ,LEN(PropertyAddress))
+
+
+
+
+
+
+SELECT *
+FROM [Portfolio Project]..Sheet1$
+
+
+
+
+
+
+
+SELECT OwnerAddress
+FROM [Portfolio Project]..Sheet1$
+
+
+SELECT
+PARSENAME(REPLACE(OwnerAddress, ',', '.') ,3)
+,PARSENAME(REPLACE(OwnerAddress, ',', '.') ,2)
+,PARSENAME(REPLACE(OwnerAddress, ',', '.') ,1)
+FROM [Portfolio Project]..Sheet1$
+
+ALTER TABLE [Portfolio Project]..Sheet1$
+Add OwnerSplitAddress Nvarchar(255);
+
+Update [Portfolio Project]..Sheet1$
+SET OwnerSplitAddress = PARSENAME(REPLACE(OwnerAddress, ',', '.') ,3)
+
+ALTER TABLE [Portfolio Project]..Sheet1$
+Add OwnerSplitCity Nvarchar(255);
+
+Update [Portfolio Project]..Sheet1$
+SET OwnerSplitCity = PARSENAME(REPLACE(OwnerAddress, ',', '.') ,2)
+
+ALTER TABLE [Portfolio Project]..Sheet1$
+Add OwnerSplitState Nvarchar(255);
+
+Update [Portfolio Project]..Sheet1$
+SET OwnerSplitState = PARSENAME(REPLACE(OwnerAddress, ',', '.') ,1)
+
+
+
+
+
+SELECT DISTINCT(SoldAsVacant), Count(SoldAsVacant)
+FROM [Portfolio Project]..Sheet1$
+GROUP BY soldAsVacant
+ORDER BY 2
+
+UPDATE [Portfolio Project]..Sheet1$
+SET SoldAsVacant = CASE When SoldAsVacant = 'Y' THEN 'Yes'
+       WHEN SoldAsVacant = 'N' THEN 'No'
+	   ELSE SoldAsVacant
+	   END
+
+
+
+
+
+-- remove dupes
+WITH RowNumCTE AS(
+SELECT *,
+    ROW_NUMBER() OVER(
+	PARTITION BY ParcelID,
+	             PropertyAddress,
+				 SalePrice,
+				 SaleDate,
+				 LegalReference
+				 ORDER BY
+				    UniqueID
+					) row_num
+
+
+
+
+FROM [Portfolio Project]..Sheet1$
+)
+SELECT *
+FROM RowNumCTE
+WHERE row_num > 1
+ORDER BY propertyaddress
+
+
+
+
+---delete unused columns
+
+
+SELECT *
+FROM [Portfolio Project]..Sheet1$
+
+
+ALTER TABLE [Portfolio Project]..Sheet1$
+DROP COLUMN SaleDate, OwnerAddress, TaxDistrict, PropertyAddress
