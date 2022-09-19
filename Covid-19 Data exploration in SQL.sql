f08c7c45a117c533cef4fec2adf72f07551b3b5b
lf-data-analysis <112306424+lf-data-analysis@users.noreply.github.com>
2022-09-19 17:13:21 +0100

    Update Covid-19 Data exploration in SQL.sql

--- Covid-19 Data exploration in SQL.sql
+++ Covid-19 Data exploration in SQL.sql
@@ -2,7 +2,6 @@
 Skills used: Joins, creating views, converting data types
 */
 
-
 SELECT *
 FROM [Portfolio Project]..CovidDeaths$
 ORDER BY 3,4
@@ -11,41 +10,34 @@ SELECT *
 FROM [Portfolio Project]..CovidVaccinations$
 ORDER BY 3,4
 
-SELECT Location, date, total_cases, new_cases,total_deaths,population
-FROM [Portfolio Project]..CovidDeaths$
-ORDER BY 1,2
-
----Percentage of total cases that result in death in the UK
-
+--This query creates a new field from two other fields to find the percentage of deaths per case for the United Kingdom
 SELECT Location, date, total_cases,total_deaths, (total_deaths/total_cases)*100 as DeathPercentage
 FROM [Portfolio Project]..CovidDeaths$
 WHERE location like 'United Kingdom'
 ORDER BY 1,2
 
----Percentage of the population who has had covid ind the UK
-
+--New field for percentage of population that has been infected.
 SELECT Location, date, total_cases, Population, (total_cases/Population)*100 as PopulationInfectionRate
 FROM [Portfolio Project]..CovidDeaths$
 WHERE location like 'United Kingdom'
 ORDER BY 1,2
 
-
----Countries with highest infection rate per capita 
-
+--Use of MAX function to filter the highest number of cases. GROUP BY is then used to find highest cases for each location.
+--ORDER BY 4 DESC sorts countries from highest number of cases to lowest.
 SELECT Location, Population, MAX(total_cases) as HighestCases, Max((total_cases/Population))*100 as PopulationInfectionRate
 FROM [Portfolio Project]..CovidDeaths$
 GROUP BY Location, population
 ORDER BY 4 DESC
 
-
----Countries with highest death rate per capita
-
+--Similar to last query but for death rate of populations.
+--WHERE clause filters out any locations which aren't countries.
 SELECT Location, Population, MAX(total_deaths) AS HighestTotalDeaths, MAX((total_deaths/Population))*100 as PopulationDeathRate
 FROM [Portfolio Project]..CovidDeaths$ WHERE continent IS NOT NULL
 GROUP BY Location, population
 ORDER BY 3 DESC
 
----Countries with highest death count
+--Countries with highest death count
+--
 SELECT Location, MAX(Cast(total_deaths AS int)) AS DeathCount
 FROM [Portfolio Project]..CovidDeaths$ WHERE continent IS NOT NULL
 GROUP BY Location
@@ -57,8 +49,7 @@ FROM [Portfolio Project]..CovidDeaths$
 GROUP BY Continent, Population
 ORDER BY 4 DESC
 
-
----Continent with highest death count
+--Sorts out data that data for anything but continents, finds max death count then orders from highest death count to lowest.
 SELECT Location, MAX(Cast(total_deaths AS int)) AS DeathCount
 FROM [Portfolio Project]..CovidDeaths$
 WHERE Continent IS NULL 
@@ -68,7 +59,7 @@ GROUP BY Location
 ORDER BY DeathCount DESC
 
 
---=Global data
+--Global data for death rate sorted by date
 SELECT date, SUM(CAST(total_cases as int)) as TotalCases , SUM(CAST(total_deaths as int)) as Totaldeaths, 
 (SUM(CAST(total_deaths as float))/SUM(CAST(total_cases as float)))*100 AS DeathPercentage
 FROM [Portfolio Project]..CovidDeaths$
@@ -76,15 +67,14 @@ WHERE continent IS NOT NULL
 GROUP BY date
 ORDER BY 1 asc
 
-
----Joining tables
+--Joining tables to access both vaccine and death data
 SELECT *
 FROM [Portfolio Project]..CovidDeaths$ dea
 JOIN [Portfolio Project]..CovidVaccinations$ vac
       ON dea.location = vac.location
 	  AND dea.date =vac.date
 
----Global New Vaccines  
+--Global New Vaccines  
 SELECT dea.date, dea.continent, dea.location, vac.new_vaccinations
 FROM [Portfolio Project]..CovidDeaths$ dea
 JOIN [Portfolio Project]..CovidVaccinations$ vac
@@ -102,7 +92,8 @@ JOIN [Portfolio Project]..CovidVaccinations$ vac
 WHERE dea.location = 'world'
 ORDER BY date
 
----Percentage of global population vaccinated
+--Using data from two joined tables. One provides data on population and the other on number of vaccinations. 
+--Using data from both tables, I create a new field for percentage of the population who has been vaccinated.
 SELECT dea.date, dea.location, vac.people_vaccinated , dea.population ,(vac.people_vaccinated/dea.population)*100 AS VaccinatedPercentage
 FROM [Portfolio Project]..CovidDeaths$ dea
 JOIN [Portfolio Project]..CovidVaccinations$ vac
@@ -111,7 +102,7 @@ JOIN [Portfolio Project]..CovidVaccinations$ vac
 WHERE dea.location = 'world'
 ORDER BY date
 
----Rolling count of vaccines by country
+--Rolling count of vaccines by country.
 set ansi_warnings off
 SELECT dea.date, dea.continent, dea.location, dea.population, vac.new_vaccinations, vac.people_vaccinated, 
 SUM(CAST(vac.new_vaccinations as bigint)) OVER (PARTITION BY dea.location ORDER BY dea.location, dea.date) AS TotalVaccinations,
@@ -124,7 +115,7 @@ WHERE dea.location LIKE 'United Kingdom'
 ORDER BY dea.location, dea.date
 
 
----another method
+--Another method for rolling count.
 WITH popvsvac(Continent, Date, Location, Population, New_vaccinations, TotalVacs)
 AS
 (
@@ -141,7 +132,7 @@ SELECT *, (totalvacs/population) AS VacsPerCapita
 FROM popvsvac
 
 
----Temp Table
+--Creating a temperary table.
 DROP TABLE IF EXISTS #percentagePopVac
 CREATE TABLE #PercentagePopVac
 (
@@ -164,14 +155,11 @@ WHERE dea.location LIKE 'United Kingdom'
 SELECT *, (RollingVaccinations/Population) AS VacsPerCapita
 FROM #PercentagePopVac
 
----Creating View we can use this view later for visualisations 
-
+--Creating View
 CREATE VIEW DeathRateByContinent as
 SELECT Location, MAX(Cast(total_deaths AS int)) AS DeathCount
 FROM [Portfolio Project]..CovidDeaths$
 WHERE Continent IS NULL 
 AND population IS NOT NULL 
-AND location NOT LIKE '%income'
-AND location NOT LIKE '%union'
-AND location NOT LIKE 'world'
+AND location NOT LIKE ('%income', '%union', 'world')
 GROUP BY Location
